5ad652bb3e716ef8227bd0ca1ff723c381d96b41
ciusji <bqjimaster@gmail.com>
2021-02-18 11:29:03 +0800

    feat: fix Rust print tag

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     // println 不是一个函数，而是一个宏
     println!("Hello, world!");
 
-    println!("Hello {0}, Hahahaha", "Rust");
+    println!("你好 {0} 2021，Rust 发光的一年。", "世界！");
 
     // 其他转义同 c 语言，这里仅需要注意 {{ 和 }}
     println!("{{}}");
