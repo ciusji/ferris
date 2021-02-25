579bdfa06479c47c1b1ea5f8aebe7bcc2dbe8746
ciusji <bqjimaster@gmail.com>
2021-02-25 14:58:21 +0800

    feat: add custom types `use` match usage 🏎

---
+++ src/example/custom_types/match_use.rs
@@ -0,0 +1,35 @@
+// Copyright 2021 Ferris Project Authors. License user Apache License.
+
+// Use
+// The use declaration can be used so manual scoping isn't needed.
+
+// An attribute to hide warnings for unused code.
+#![allow(dead_code)]
+
+enum Status {
+    Rich,
+    Poor,
+}
+
+enum Work {
+    Civilian,
+    Soldier,
+}
+
+pub fn match_use() {
+    use crate::Status::{ Poor, Rich };
+    use crate::Work::*;
+
+    let status = Poor;
+    let work = Civilian;
+
+    match status {
+        Rich => println!("The rich have lots of money!"),
+        Poor => println!("The poor have no money..."),
+    }
+
+    match work {
+        Civilian => println!("Civilian work!"),
+        Soldier => println!("Soldiers fight!"),
+    }
+}
