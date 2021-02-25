16a276671b835b8823f7f844cefac8f3df7b0fbd
ciusji <bqjimaster@gmail.com>
2021-02-25 16:00:44 +0800

    feat: add variable bindings usages 🎳

--- src/example/variable_bindings/bindings.rs
+++ src/example/variable_bindings/bindings.rs
@@ -13,4 +13,16 @@ pub fn bindings() {
     mutable_binding += 1;
     println!("After mutation: {}", mutable_binding);
 
+    // Declare a variable binding
+    let a_binding;
+    {
+        let x = 2;
+        a_binding = x * x;
+    }
+    println!("a binding: {}", a_binding);
+
+    // let another_binding;
+    // error[E0282]: type annotations needed
+    // println!("another binding: {}", another_binding);
+
 }
