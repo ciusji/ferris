01012831207290bdc5c727dedfd33e059eb58d81
ciusji <bqjimaster@gmail.com>
2021-02-26 10:37:10 +0800

    feat: fix expression `;` issue

--- src/example/expressions/expression.rs
+++ src/example/expressions/expression.rs
@@ -1,12 +1,15 @@
 // Copyright 2021 Ferris Project Authors. License user Apache License.
 
 // Expressions
+// Blocks are expression too, so they can be used as values in assignments. The last expression in
+// the block will be assigned to the place expression such as a local variable. However, if the last
+// expression of the block ends with a semicolon, the return value will be ().
 
 pub fn expression() {
     let x = 5u32;
 
     let y = {
-        let x_squared = x^2;
+        let x_squared = x ^ 2;
         let x_cube = x_squared * x;
         // This expression will be assigned to `y`
         x_cube + x_squared + x
@@ -20,4 +23,5 @@ pub fn expression() {
     println!("x is {:?}", x);
     println!("y is {:?}", y);
     println!("z is {:?}", z);
+
 }
