c25665f4331c94c0f15c256d4a166a947fa2aca1
ciusji <bqjimaster@gmail.com>
2021-03-03 14:02:13 +0800

    feat: add std String demo

--- src/example/std_libs/strings.rs
+++ src/example/std_libs/strings.rs
@@ -13,4 +13,51 @@ pub fn strings() {
     let pangram: &'static str = "the quick brown fox jumps over the lazy dog";
     println!("Pangram: {}", pangram);
 
+    // Iterate over words in reverse, no new string is allocated
+    println!("Words in reverse");
+    for word in pangram.split_whitespace().rev() {
+        println!("> {}", word);
+    }
+
+    // Copy charts into a vector, sort and remove duplicates
+    let mut chars: Vec<char> = pangram.chars().collect();
+    chars.sort();
+    chars.dedup();
+
+    for xx in chars.iter() {
+        print!("{}", xx);
+    }
+    println!();
+
+    // Create an empty and growable `String`
+    let mut string = String::new();
+    for c in chars {
+        // Insert a char at the end of string
+        string.push(c);
+        // Insert a string at the end of string
+        string.push_str(", ");
+    }
+
+    println!("Current String is: {:?}", string);
+
+    // The trimmed string is a slice to the original string, hence no new allocation is performed.
+    let chars_to_trim: &[char] = &[' ', ','];
+    let trimmed_str: &str = string.trim_matches(chars_to_trim);
+    println!("Used characters: {}", trimmed_str);
+
+    // Heap allocate a string
+    let alice = String::from("I like dogs");
+    // Allocate new memory and store the modify string here
+    let bob: String = alice.replace("dog", "cat");
+
+    println!("Alice says: {}", alice);
+    println!("Bob says: {}", bob);
+
+    let _long_string = "String literals
+                                can span multiple lines
+                                        The linebreak and indentation here -> |
+                        <- can be escaped too!
+        ";
+    println!("{}", _long_string);
+
 }
