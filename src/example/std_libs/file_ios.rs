99fc9b1c87b3163622939d99364d6f5640ed71b2
ciusji <bqjimaster@gmail.com>
2021-03-04 19:05:20 +0800

    feat: update std io file ops

--- src/example/std_libs/file_ios.rs
+++ src/example/std_libs/file_ios.rs
@@ -7,10 +7,20 @@
 
 use std::fs::File;
 use std::path::Path;
-use std::io::Read;
+use std::io::{Read, Write};
+use std::io;
+
+static LOREM_IPSUM: &str =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+";
 
 fn open_file() {
-    // Create a path to the desired file
+    // Create a path to the desired file（panic! No such file or directory）
     let path = Path::new("/Users/admin/Desktop/rust_programming.txt");
     let display = path.display();
 
@@ -32,6 +42,24 @@ fn open_file() {
     // `file` goes out of scope, and the "*.txt" file gets closed
 }
 
+fn file_create() {
+    let path = Path::new("/Users/admin/Desktop/lorem_ipsum.txt");
+    let display = path.display();
+
+    // Open a file in write-only mode, return `io::Result<File>`
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Write the `LOREN_IPSUM` string to `file`, returns `io::Result<()>`
+    match file.write_all(LOREM_IPSUM.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
+}
+
 pub fn file_ops() {
     open_file();
+    file_create();
 }
