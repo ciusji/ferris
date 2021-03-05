53595991f06fabee935a6eb2161dadbbdc00db47
ciusji <bqjimaster@gmail.com>
2021-03-05 13:25:49 +0800

    fix: fix cargo.toml, add edition 2018 arg

--- src/example/cli/grrs.rs
+++ src/example/cli/grrs.rs
@@ -1,13 +1,14 @@
 //! Copyright 2021 Ferris Project Authors. License user Apache License.
 
-use std::env::args;
-use std::path::PathBuf;
-
 use structopt::StructOpt;
 
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
-    // Aside: `PathBuf` is like a `String` but for file system paths that works cross-platform.
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
@@ -23,15 +24,21 @@ struct Cli {
 /// foo: 10
 /// ```
 pub fn grrs() {
-    // Getting the arguments
-    let pattern = args().nth(1).expect("no pattern given");
-    let path = args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern: pattern,
-        path: PathBuf::from(path),
-    };
+    // // Getting the arguments
+    // let pattern = args().nth(1).expect("no pattern given");
+    // let path = args().nth(2).expect("no path given");
+    // let args = Cli {
+    //     pattern,
+    //     path: PathBuf::from(path),
+    // };
+    //
+    // println!("{:?}", args.pattern);
 
-
-
-
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
