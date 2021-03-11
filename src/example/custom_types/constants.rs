4835698420324018367d4043d1cb228fba66ad5c
ciusji <bqjimaster@gmail.com>
2021-03-11 10:34:39 +0800

    comm: diff rust global variable.

--- src/example/custom_types/constants.rs
+++ src/example/custom_types/constants.rs
@@ -1,6 +1,12 @@
 // Copyright 2021 Ferris Project Authors. License user Apache License.
 
 // Constants
+// Rust has two different types of constants which can be declared in any scope including global.
+// Both require explict type annotation.
+// - const: An unchangeable value (the common case).
+// - static: A possible mutable variable with 'static' lifetime. The static lifetime is inferred and
+//      does not have to be specified. Accessing or modifying a mutable static variable is unsafe.
+// See also: 'static lifetime, The const/static RFC
 
 // Globals are declared outside all other scopes.
 static LANGUAGE: &str = "Rust";
