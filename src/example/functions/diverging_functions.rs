924bf18490ce72f420efaeaeb29b8754b83241a4
ciusji <bqjimaster@gmail.com>
2021-03-11 11:09:31 +0800

    comm: diverge function

--- src/example/functions/diverging_functions.rs
+++ src/example/functions/diverging_functions.rs
@@ -1,4 +1,12 @@
 // Copyright 2021 Ferris Project Authors. License user Apache License.
 
-// Diverging functions never return. They are marked using !, which is an empty type.
+// Diverging Function (发散函数)
+// Diverging functions never return. They are marked using `!`, which is an empty type.
 
+fn foo() -> ! {
+    panic!("This call never returns.")
+}
+
+// As opposed to all the other types, this one cannot be instantiated, because the set of all possible
+// values this type can have is empty. Note that, it is different form the `()` type, which has exactly
+// one possible value.
